2e26a05f0a197fd9021228fb07f733bd754edc27
Walden Yan <waldenyan20@gmail.com>
2022-07-25 12:50:14 -0400

    feat: first commit

---
+++ contracts/VaultBasic.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.6.0;
+
+contract VaultBasic {
+    mapping(address => uint256) public balance;
+
+    function deposit() public payable {
+        balance[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 amt) public {
+        require(balance[msg.sender] >= amt);
+        balance[msg.sender] -= amt;
+        (bool success, ) = msg.sender.call{value: amt}("");
+        if (!success) {
+            balance[msg.sender] += amt;
+        }
+    }
+}
